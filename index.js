5f537e8eca1fc3a200bb496c0461af9e674d4779
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-01-08 05:38:28 +0600

    Committed

---
+++ index.js
@@ -0,0 +1,10 @@
+//import {json-server}
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.port || 5000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
